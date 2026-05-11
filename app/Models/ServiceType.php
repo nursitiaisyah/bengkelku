4649276b0ed2931df26fe1c584a8a95629cbbb49
Nur Siti Aisyah <nur617859@gmail.com>
2026-05-11 09:59:57 +0700

    add model migration for service type

---
+++ app/Models/ServiceType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceType extends Model
+{
+    protected $fillable = [
+        'image',
+        'name',
+        'description'
+    ];
+}
