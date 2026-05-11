34a05ae0ea985674870fa70b1a736c91ec1231ad
Nur Siti Aisyah <nur617859@gmail.com>
2026-05-11 09:50:38 +0700

    feat auth

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthenticateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // Login
+    public function login(AuthenticateUserRequest $request)
+    {
+        $credentials = $request->validated();
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return ApiResponse::error(
+                'Invalid credentials',
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return ApiResponse::success(
+            [
+                'token' => $token,
+                'user' => new UserResource($user)
+            ],
+            'Login successfull'
+        );
+    }
+
+    // Get Profile
+    public function me(Request $request)
+    {
+        return ApiResponse::success(
+            new UserResource($request->user()),
+            'User data'
+        );
+    }
+
+    // Logout
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return ApiResponse::success(
+            null,
+            'Logout successfull'
+        );
+    }
+}
